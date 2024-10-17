ce2c357fe0a3bb077c688e8422d98b48b7e670ad
Willaim-dias <williampacifico41@gmail.com>
2024-10-16 21:33:43 -0300

    graph test Part 1

--- src/view/GraphicsViewController.java
+++ src/view/GraphicsViewController.java
@@ -2,13 +2,37 @@ package view;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
 
 public class GraphicsViewController implements Initializable {
 
+    @FXML
+    private PieChart pieChartExpenses;
+
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+       addDataGraphic();
     }    
     
+    private void addDataGraphic() {
+        ObservableList<PieChart.Data> pieCharData = FXCollections.observableArrayList(
+           new PieChart.Data("Agua",56.90),
+           new PieChart.Data("Luz",130)
+        );
+        
+        pieCharData.forEach(data -> data.nameProperty().bind(
+                Bindings.concat (
+                        data.getName(),":",data.pieValueProperty()
+                )
+        ));
+        
+        pieChartExpenses.getData().addAll(pieCharData);
+    }
 }
